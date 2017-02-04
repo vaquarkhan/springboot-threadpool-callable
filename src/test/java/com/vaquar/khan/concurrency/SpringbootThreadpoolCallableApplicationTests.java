d72d9e2cf04d7895dcde08ecbfaab028b489596e
v.khan@zipcomps.com <vaquar.khan@gmail.com>
2017-02-04 13:24:37 -0600

    commit

--- src/test/java/com/vaquar/khan/concurrency/SpringbootThreadpoolCallableApplicationTests.java
+++ src/test/java/com/vaquar/khan/concurrency/SpringbootThreadpoolCallableApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.vaquar.khan.concurrency.SpringbootThreadpoolCallableApplication;
-
+/**
+*
+*/
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringbootThreadpoolCallableApplication.class)
 public class SpringbootThreadpoolCallableApplicationTests {
